a2885cc8bb81638f62ec6b28faa5f3cc4bca21a3
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-21 22:33:28 -0700

    basic rendering with styles

--- ei/src/App.js
+++ ei/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import './styles/App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      feeling: 'happy'
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
+        <h1 className="App-main-face">😊</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Happy coding!
         </p>
       </div>
     );
