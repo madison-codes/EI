5be667f971d7386e855f504759249e751a35d9de
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-21 23:04:07 -0700

    basic rendering

--- ei/src/App.js
+++ ei/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
+// you need to import elements from other components & is rendered on ln 23
+import Controls from './Controls';
 import './styles/App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
+      // when you change this value the screen will rerender
       feeling: 'happy'
     };
   }
 
   render() {
     return (
-      <div className="App">
+      // this is a ternary expression (conditional) checking if there is a state - if there is not the class will default to App, as seen after the colon
+      <div className={this.state.feeling ? this.state.feeling : "App"}>
         <h1 className="App-main-face">😊</h1>
         <p className="App-intro">
           Happy coding!
         </p>
+        <Controls />
       </div>
     );
   }
---
+++ ei/src/Controls.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+// whatever you pass in from the parent app will be passed into each indiv. component as props
+// to call methods use props.insertMethodName (should be after the dot and called if method)
+// in this case we only want it to return an element so we will export only the JSX - once you add functionalities to the buttons you will call the methods from here. You may have to change the structure of the component, ln 8.
+
+export default (props) => {
+    return(
+      <section className='control-container'>
+        <button
+          className='control-voice'
+          onClick={() => console.log('voice')}>
+          Voice
+        </button>
+        <button
+          className='control-video'
+          onClick={() => console.log('video')}>
+          Video
+        </button>
+      </section>
+    )
+  }
